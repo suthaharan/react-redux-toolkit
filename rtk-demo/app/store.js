cc63713b653f4421a187aa3ba62ab23239f4d115
sutha <sutha@kurinchilion.com>
2023-05-02 23:39:25 -0400

    adding in biscuit slice and actions

--- rtk-demo/app/store.js
+++ rtk-demo/app/store.js
@@ -1,9 +1,11 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
 const cakeReducer = require('../features/cake/cakeSlice')
+const biscuitReducer = require('../features/biscuit/biscuitSlice')
 
 const store = configureStore({
     reducer:{
         cake: cakeReducer,
+        biscuit: biscuitReducer,
     }
 })
 
--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const biscuitActions = require('./features/biscuit/biscuitSlice').biscuitActions
 
 console.log('Initial state ', store.getState())
 
@@ -9,5 +10,8 @@ const unsubscribe = store.subscribe(() => {
 
 // create an action
 store.dispatch(cakeActions.ordered());
-
+store.dispatch(cakeActions.restocked(2));
+store.dispatch(biscuitActions.ordered());
+store.dispatch(biscuitActions.ordered());
+store.dispatch(biscuitActions.restocked(1));
 unsubscribe();
